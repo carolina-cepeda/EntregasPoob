fb4f77a316341ee9e72e96f0a6bebb74148c13e3
JuanitaRubiano <juanita.rubiano-f@mail.escuelaing.edu.co>
2025-03-07 11:51:49 -0500

    upload ciclo 1

---
+++ shapes/MAXWELL JRCC/MaxwellContainer.java
@@ -0,0 +1,11 @@
+
+import java.awt.*;
+/**
+ * Write a description of class MaxwellContainer here.
+ * 
+ * @author Juanita Rubiano  
+ * @version 4/03/2025
+ */
+public class MaxwellContainer{
+    
+}
