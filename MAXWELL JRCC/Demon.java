70bcacdca11cb5a36c246511278f34871e61f29b
carolina-cepeda <carolina.cepeda-v@mail.escuelaing.edu.co>
2025-03-08 16:45:00 -0500

    funcional
    cambio c a d

--- MAXWELL JRCC/Demon.java
+++ MAXWELL JRCC/Demon.java
@@ -28,9 +28,9 @@ public class Demon
     public boolean pasar( Particle p){
         int posx = p.getpX();
         int posy = p.getpY();
-        int posCX = triangulo.getXposition();
-        int posCY = triangulo.getYposition();
-        return ( posx==posCX && posy == posCY) ;
+        int posDX = triangulo.getXposition();
+        int posDY = triangulo.getYposition();
+        return ( posx==posDX && posy == posDY) ;
     }
     public void makeVisible(){
         triangulo.makeVisible();
