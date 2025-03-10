1d545b05e8860843d223acb62f93819b0462b42e
carolina-cepeda <carolina.cepeda-v@mail.escuelaing.edu.co>
2025-03-10 18:24:53 -0500

    update contenedor

--- MAXWELL JRCC/Tablero.java
+++ MAXWELL JRCC/Tablero.java
@@ -10,9 +10,11 @@ public class Tablero
 {
     private MaxwellContainer MaxwellContainer;
     private Canvas canvas;
+    private Rectangle marco, separacion, interior;
     private TreeMap <String,Hole> holes;
     private TreeMap <String,Particle> particles;
     private TreeMap <String,Demon> demons;
+    
     /**
      * Constructor for objects of class Tablero based in canvas.
      */
@@ -20,7 +22,38 @@ public class Tablero
     {
         canvas = Canvas.getCanvas();
         
+        marco = new Rectangle();
+        marco.changeSize(h,w);
+        marco.changeColor("red");
+        interior = new Rectangle();
+        interior.changeSize(h-8,w-8);
+        interior.moveHorizontal(4);
+        interior.moveVertical(4);
+        
+        
+        separacion = new Rectangle();
+        separacion.changeSize(h,2);
+        separacion.changeColor("black");
+        separacion.moveHorizontal(w/2);
+        
+        
+        
     }
+    
+    public void makeVisible(){
+        marco.makeVisible();
+        interior.makeVisible();
+        separacion.makeVisible();
+        
+    }
+    
+    public void makeInvisible(){
+        marco.makeInvisible();
+        interior.makeInvisible();
+        separacion.makeInvisible();
+
+    }
+    
     
     
 
