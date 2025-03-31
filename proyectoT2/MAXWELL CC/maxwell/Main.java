788309da8da6f41e62bc01b774f0745c2fceb564
carolina-cepeda <carolina.cepeda-v@mail.escuelaing.edu.co>
2025-03-30 19:53:19 -0500

    update faltan pruebas aceptacion
    se traba en la mitad

--- proyectoT2/MAXWELL CC/maxwell/Main.java
+++ proyectoT2/MAXWELL CC/maxwell/Main.java
@@ -3,13 +3,14 @@ package maxwell;
 public class Main {
     public static void main(){
         int[][] particulas={
-            {80,85,8,3},
-            {-30,100,7,5},
-            {82,120,4,5}
+            {80,85,2,2},
+            {-30,100,2,2},
+            {82,120,2,2}
     };
      MaxwellContainer mc = new MaxwellContainer(200,200,85,1,1,particulas);
-     mc.addHole(25,80,1);
+     mc.addParticle("sienna",false,-80,83,0,-3);
+     mc.addHole(-80,80,1); //hole sirve
      mc.makeVisible();
-     mc.start(20);
+     mc.start(200);
 }
 }
